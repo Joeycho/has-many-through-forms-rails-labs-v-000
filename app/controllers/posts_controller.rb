bc36427edfbdf9a604433fbe2f73441d8622fdbd
Joey Cho <cjy12182000@naver.com>
2019-02-24 02:13:12 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new(post: @post)
   end
 
   def index
@@ -12,6 +14,7 @@ class PostsController < ApplicationController
   end
 
   def create
+
     post = Post.create(post_params)
     redirect_to post
   end
